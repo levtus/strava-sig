2a2520745bb535bfef08478ae0e1682493f43cc3
levtus <61996958+levtus@users.noreply.github.com>
2023-04-23 14:38:29 -0500

    work on script

--- strava.js
+++ strava.js
@@ -1,21 +1,10 @@
 const clientId = '98135';
 const clientSecret = '250fb83eda23244fd4a165a4a8565f398a5e1e56';
 var code;
-var activities = [];
 var userData
 var data
 var accessToken
-var startDate = 0;
-var endDate = (Date.now() / 1000 );
-var displayAmount
-var opacity = 0.9;
-var mapColor = "#3289c7"
-var activityPurpose = "All Activities";
-var activityType = "All Activities";
-var mapStyle = "Single Color"; 
-var wantedStartDate = 0;
-var wantedEndDate = 99999999999999;
-
+var activityData
 
 window.addEventListener("load", (event) => {
     getURLCode()
@@ -26,11 +15,6 @@ window.addEventListener("load", (event) => {
         console.log('Code Not Present on Load') 
     }
 });
-
-async function main() {
-    await getActivities()
-    await displayRides()
-}
 
 // Redirect the user to the Strava authorization page
 function authenticate() {
@@ -45,6 +29,8 @@ function authenticate() {
 $("#connectButton").click(function () {
     authenticate()
 })
+
+
 
 // Get the authorization code from the URL
 function getURLCode() {
@@ -81,119 +67,57 @@ async function getAccessToken(code) {
   return accessToken;
 }
 
-async function getActivities() {
-    const displayCount = parseInt(document.getElementById("displayCount").value);
-    const pageCount = Math.ceil(displayCount / 100);
-    for (let i = 0; i < pageCount ; i++) {
-        const page = i + 1
-        console.log(`Getting Page ${page}`)
-        const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&per_page=100&page=${page}`;
-        const response = await fetch(activitiesLink);
-        const activityData = await response.json();
-        activities = activities.concat(activityData);
+async function getUserData() {
+    if (!userData) {
+        const apiUrl = 'https://www.strava.com/api/v3/athlete';
+        const response = await fetch(apiUrl, {
+            headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            },
+        });
+        userData = await response.json();
+        console.log("User Profile Information: " + userData);
+        return userData;
+    } else {
+        console.log("User profile information already present: " + userData)
     }
-    console.log(activities);
+}
+
+async function getUserStats(id) {
+    if (!activityData) {
+        const apiUrl = `https://www.strava.com/api/v3/athletes/${id}/stats`;
+        const response = await fetch(apiUrl, {
+            headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            },
+        });
+        userStats = await response.json();
+        console.log("User Activity Statistics: " + userStats);
+        return userStats;
+    } else {
+        console.log("User Activity Statistics Already Present: " + activityData)
+    }
 }
 
 
+function printAthleteInfo() {
+    getUserData();
+    const id = userData.id;
+    const username = userData.username;
+    const name = `${userData.firstname} ${userData.lastname}`;
+    const premiumStatus = userData.premium
+    const creationDate = formatDate(userData.created_at)
+    const picture = userData.profile
+    const athleteType = userData.athlete_type
+    const unit = userData.measurement_preference
+    getUserStats(id);
+    const maxRide = userStats.biggest_ride_distance
+    const rideStats = userStats.all_ride_totals
+    const runStats = userStats.all_run_totals
+    console.log(`All-Time Cycling Stats: ${rideStats}`)
+    console.log(`All-Time Running Stats: ${runStats}`)
 
-function displayRides() {
-    let firstActivityTimestamp = 0;
-    let lastActivityTimestamp = 0;
-    
-    for (let i = 0; i < activities.length; i++) {
-        const coordinates = L.Polyline.fromEncoded(activities[i].map.summary_polyline).getLatLngs();
-        const distance = (Math.round(activities[i].distance / 100) / 10);
-        const typeName = activities[i].sport_type.replace(/([a-z])([A-Z])/g, '$1 $2');
-        const epochDate = Date.parse(activities[i].start_date_local) / 1000
-        const formattedDate = formatDate(activities[i].start_date_local);
-        
-        const elapsedTime = formatTime(activities[i].elapsed_time);
-        const movingTime = formatTime(activities[i].moving_time);
-        const activityName = activities[i].name;
-        
-        if (document.getElementById('mapStyle').value == "Recency") {
-            if ( i < 75 ) {
-                opacity = 1 - (i * 0.01)
-            } else {
-                opacity = 0.25
-            }
-        };
-        
-        if (document.getElementById('mapStyle').value == "Random Color") {
-            var randomColor = Math.floor(Math.random()*16777215).toString(16);
-            mapColor = "#" + randomColor;
-        };
-        
-        if (matchActivityPurpose(i) == true && matchActivityType(i) == true && matchPrivStatus(i) == true && matchDateStatus(i) == true) {
-                console.log(activityName)
-                L.polyline(
-                    coordinates,
-                    {
-                        color: mapColor,
-                        weight: 4,
-                        opacity: opacity,
-                        lineJoin:'round'
-                    }
-                ).bindPopup(`
-                    <p class="activityTitle">${activityName}</p>
-                    <p class="date">${formattedDate}</p>
-                    <p>Moving Time: ${movingTime}</p>
-                    <p>Distance: ${distance}km</p>
-
-                    <a href=\"https://www.strava.com/activities/${activities[i].id}\" targe t="_blank">View on Strava</a>
-                `).on('click', function(e) {
-                    var layer = e.target;
-                    layer.setstyle({
-                        color: '#e2eb02',
-                        opacity: opacity,
-                        weight: 4
-                    });
-                    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-                        layer.bringToFront();
-                    }                    
-                    map.fitBounds(layer.getBounds());
-                    layer.setStyle({
-                        color: '#e2eb02',
-                        opacity: 1,
-                        weight: 8
-                    });
-                }).on('mouseover', function(e) { 
-                    var layer = e.target;
-                    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-                        layer.bringToFront();
-                    }  
-                    layer.setStyle({
-                        color: '#e2eb02',
-                        opacity: opacity,
-                        weight: 8
-                    });
-                }).on('mouseout', function(e) {
-                    var layer = e.target;
-                    if (document.getElementById('mapStyle').value == "Random Color") {
-                        var randomColor = Math.floor(Math.random()*16777215).toString(16);
-                        mapColor = "#" + randomColor;
-                    };
-                    layer.setStyle({
-                        color: mapColor,
-                        opacity: opacity,
-                        weight: 4
-                    });
-                }).addTo(traces)
-                document.getElementById("activityListContainer").innerHTML = document.getElementById("activityListContainer").innerHTML + `<a class="activityListItem" href="https://www.strava.com/activities/${activities[i].id}" targe t="_blank"">${i + 1}. ${activityName}</a><br>`
-    }
-    };
-    if (mapStyle == "Heatmap") {
-        var heatmap = L.webGLHeatmap({size: 100, opacity: 0.5}); 
-        heatmap.setData(traces)
-        map.addLayer(heatmap);
-        console.log("Created Heatmap")
-        map.fitBounds(traces.getBounds());
-    } else {
-        traces.addTo(map)
-        map.fitBounds(traces.getBounds());
-    }
-}            
+}
 
 function formatDate(notFormatted) {
     const date = new Date(notFormatted);
@@ -209,145 +133,6 @@ function formatTime(seconds) {
     const time = new Date(seconds);
     time.setSeconds(time);
     return time.toISOString().substr(11, 8)
-}
-
-function removeParamFromURL() {
-  const urlObj = new URL(location); 
-  urlObj.searchParams.delete('state');
-  urlObj.searchParams.delete('code');
-  urlObj.searchParams.delete('scope');
-  window.location.href = urlObj.toString();
-  return urlObj.toString();
-}
-
-function updateStartDate() {
-    wantedStartDate = new Date(document.getElementById('startDate').value).getTime();
-    console.log(`Updated Start Date to ${wantedStartDate}`)
-}
-
-function updateEndDate() {
-    wantedEndDate = new Date(document.getElementById('endDate').value).getTime();
-    console.log(`Updated Start Date to ${wantedEndDate}`)
-}
-
-function updateMapType() {
-    mapStyle = document.getElementById('mapStyle').value;
-    if (mapStyle == "Random Color") {
-        document.getElementById('picker').style.display = "none";
-        document.getElementById('traceColor').style.display = "none";
-    } else {
-        document.getElementById('picker').style.display = "table";
-        document.getElementById('traceColor').style.display = "table";
-    }
-    console.log(`Updated Map Type to ${mapStyle}`)
-}
-
-function matchDateStatus(activityNumber) {
-    const activityDate = Date.parse(activities[activityNumber].start_date_local);
-    console.log(`Activity Timestamp ${activityDate}`)
-    if (activityDate > wantedStartDate  && activityDate < wantedEndDate) {
-        console.log("Date Check Successful")
-        return true;
-    } else {
-        console.log("Date Check Failed")
-        return false;
-    }
-}
-
-function matchPrivStatus(activityNumber) {
-    const activityPrivateFilter = document.getElementById('privateActivities').checked;
-    const realActivitySetting = activities[activityNumber].private;
-    
-    if ( activityPrivateFilter == true) {
-        console.log("Private Check Successful")
-        return true;
-    } else if ( realActivitySetting == false ) {
-        console.log("Private Check Successful")
-        return true;
-    } else {
-        console.log("Private Check Failed")
-        return false;
-    }
-}
-
-function matchActivityType(activityNumber) {
-    const activityTypeFilter = document.getElementById('activityType').value;
-    const realActivityType = activities[activityNumber].type;
-
-    if (activityTypeFilter == "All Activities") {
-        console.log("Activity Type Check Successful")
-        return true;
-    } else if (activityTypeFilter == "Runs/Hikes/Walks") {
-        if (realActivityType == "Run" || realActivityType == "Walk" || realActivityType == "Hike" ) {
-            console.log("Activity Type Check Successful")
-            return true;
-        } else {
-            console.log("Activity Type Check Failed")
-            return false;
-        }
-    } else if (activityTypeFilter == "Rides") {
-        if (realActivityType == "Ride" || realActivityType == "EBikeRide" || realActivityType == "Handcycle" || realActivityType == "Velomobile" ) {
-            console.log("Activity Type Check Successful")
-            return true;
-        } else {
-            console.log("Activity Type Check Failed")
-            return false;
-        }
-    } else if (activityTypeFilter == "Skiing/Skating" ) {
-        if ( realActivityType == "AlpineSki" || realActivityType == "BackcountrySki" || realActivityType == "NordicSki" || realActivityType == "RollerSki" || realActivityType == "Snowboard" || realActivityType == "IceSkate" || realActivityType == "InlineSkate" || realActivityType == "Skateboard" || realActivityType == "Wheelchair" ) {
-            console.log("Activity Type Check Successful")
-            return true;
-        } else {
-            console.log("Activity Type Check Failed")
-            return false;
-        }
-    } else if (activityTypeFilter == "Water Sports" ) {
-        if (realActivityType == "Canoeing" || realActivityType == "Kayaking" || realActivityType == "Kitesurf" || realActivityType == "Rowing" || realActivityType == "Sail" || realActivityType == "StandUpPaddling" || realActivityType == "Surfing" || realActivityType == "Swim" || realActivityType == "Windsurf" ) {
-            console.log("Activity Type Check Successful")
-            return true;
-        } else {
-            console.log("Activity Type Check Failed")
-            return false;
-        }
-    } else {
-        console.log("Couldn't determine wanted activity type filter.")
-        return true;
-    }
-}
-function matchActivityPurpose(activityNumber) {
-    const activityPurposeFilter = document.getElementById('activityPurpose').value;
-    const isCommute = activities[activityNumber].commute;
-
-    if (activityPurposeFilter == "All Activities") {
-        console.log("Commute Check Successful")
-        return true;
-    } else if (activityPurposeFilter == "No Commutes") {
-        if (isCommute == true) {
-            console.log("Commute Check Failed")
-            return false;
-        } else {
-            console.log("Commute Check Successful")
-            return true;
-        }
-    } else if (activityPurposeFilter == "Only Commutes") {
-        if (isCommute == true) {
-            console.log("Commute Check Successful")
-            return true;
-        } else {
-            console.log("Commute Check Failed")
-            return false;
-        } 
-    } else {
-        console.log("Couldn't determine wanted activity purpose filter.")
-        return true;
-    }
-}
-
-function exportGPX() {
-    const output = L.ConvertCoords.GPX.convert(traces);
-    const fileName = "Strava Traces.gpx";
-    saveAs(new Blob([output], {type: "text/xml;charset=utf-8;"}), fileName);
-    console.log("Exported GPX")
 }
 
 function showViewCount() {
